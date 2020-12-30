5613bc60e593f9d0247e73b42cd27ee3a220603f
jungkeunmo <58620940+jungkeunmo@users.noreply.github.com>
2020-12-30 16:35:01 +0900

    js

--- src/server.js
+++ src/server.js
@@ -2,7 +2,6 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import morgan from "morgan";
-import mongoose from "mongoose";
 import globalRouter from "./router/globalRouter";
 import connect from "../db/mongo";
 
@@ -12,7 +11,6 @@ const app = express();
 app.use(morgan(`dev`));
 app.set("view engine", "pug");
 connect();
-mongoose.connect();
 
 app.use("/", globalRouter);
 
